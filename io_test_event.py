b0d4b1b8dd41a132dfcb46a50f951c932d1d1ecf
Ed Schneider <ed4prez@yahoo.com>
2017-12-10 21:51:55 -0600

    It's working!!!

--- io_test_event.py
+++ io_test_event.py
@@ -14,23 +14,30 @@ R_R_Status = 1
 
 New_Input = False
 
+# Key_Down_Stamp = dt.datetime.now()
 
 def callback_button(inputpin):
-    if GPIO.input(input) == 0:
-        Key_Down_Stamp = dt.datetime.now()
-    else:
-        New_Input = True
+    print (GPIO.input(inputpin))
 
+    global Key_Down_Stamp, New_Input, Key_Press
+
+
+    if GPIO.input(inputpin) == 1:
+        New_Input = True
         if inputpin == B_L:
             Key_Press = "L"
         elif inputpin == R_R:
             Key_Press = "R"
 
-        if (datetime.datetime.now() - Key_Down_Stamp) > datetime.timedelta(seconds=1):
+        if (dt.datetime.now() - Key_Down_Stamp) > dt.timedelta(seconds=1):
             New_Input = "Long"
         else:
             New_Input = "Short"
-        Menu_Revert_Time = datetime.datetime.now()  # + TimeStamp_Check
+      #  Menu_Revert_Time = datetime.datetime.now()  # + TimeStamp_Check
+        print (Key_Press,New_Input)
+    else:
+    #    GPIO.input(inputpin) == 0:
+        Key_Down_Stamp = dt.datetime.now()
 
 
 GPIO.add_event_detect(B_L, GPIO.BOTH, callback=callback_button)
@@ -38,8 +45,8 @@ GPIO.add_event_detect(R_R, GPIO.BOTH, callback=callback_button)
 
 while True:
     if New_Input != False:
-        print(Key_Press)
-        print(New_Input)
+        print("KP",Key_Press)
+        print("NI",New_Input)
         New_Input = False
 
 GPIO.cleanup()
